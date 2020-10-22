d670683ae0308c9d8b829c7fcbf37aba119653b5
Baljeet Jangra <baljeetjangraa@gmail.com>
2020-10-22 19:17:52 +0530

    project setup

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    dark_navy: "#020c1b",
+    navy: "#0a192f",
+    light_navy: "#172a45",
+    lightest_navy: "#303c55",
+    navy_shadow: "rgba(2, 12, 27, 0.7)",
+    slate: "#8892b0",
+    light_slate: "#a8b2d1",
+    lightest_slate: "#ccd6f6",
+    white: "#e6f1ff",
+    green: "#64ffda",
+    green_tint: "rgba(100, 255, 218, 0.1)",
+  },
+};
+
+export default theme;
